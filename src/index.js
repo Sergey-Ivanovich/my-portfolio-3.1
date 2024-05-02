77519b52d044b58f47d20c54e67a7358ae0e863e
Sergey-Ivanovich <ericnoata@gmail.com>
2024-05-02 19:54:24 +0100

    began working on responsive mobile navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+function toggleMobileNav() {
+  mobileNavElement.classList.toggle("hide-nav");
+}
 
+let mobileNavElement = document.querySelector("#mobile-nav");
+let hamburgerMenuToggle = document.querySelector(".hamburger-menu");
+hamburgerMenuToggle.addEventListener("click", toggleMobileNav);
