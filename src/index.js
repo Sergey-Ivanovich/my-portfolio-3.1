54f5008e59fd9989fd83d7295e67a67b8a1b043c
Sergey-Ivanovich <ericnoata@gmail.com>
2024-05-09 18:32:15 +0100

    improved responsive ui

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ menuToggle.addEventListener("click", toggleNav);
 if (window.innerWidth < 767) {
   toggleNav();
   setTimeout(toggleNav, 800);
-  setTimeout(toggleNav, 1600);
+  setTimeout(toggleNav, 1100);
 }
