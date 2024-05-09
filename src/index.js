d014f0b9df1f58b5aa3beb56cf7d65a118729ce6
Sergey-Ivanovich <ericnoata@gmail.com>
2024-05-09 18:22:21 +0100

    aligned header content to middle

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ let mobileNavElement = document.querySelector("#navbar");
 let menuToggle = document.querySelector(".nav-arrow");
 menuToggle.addEventListener("click", toggleNav);
 
-toggleNav();
-
 if (window.innerWidth < 767) {
+  toggleNav();
   setTimeout(toggleNav, 800);
   setTimeout(toggleNav, 1600);
 }
