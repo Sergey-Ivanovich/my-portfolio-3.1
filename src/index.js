c57c76c80f4730538def23b11914b22c66222b54
Sergey-Ivanovich <ericnoata@gmail.com>
2024-05-28 17:27:21 +0100

    made drop down function smoothly

--- src/index.js
+++ src/index.js
@@ -27,7 +27,22 @@ dropDownElements.forEach((dropDown, index) => {
 });
 
 let workMasterElement = document.querySelector(".master-work-container");
+let hiddenElement = document.querySelector(".hidden-projects");
+
 let workExpanderElement = document.querySelector(".expand-toggle");
-workExpanderElement.addEventListener("click", () => {
-  workMasterElement.classList.toggle("expanded");
-});
+
+const hiddenHeight = hiddenElement.clientHeight + 70;
+
+workExpanderElement.addEventListener("click", expandHiddenProjects);
+
+function expandHiddenProjects() {
+  console.log(hiddenHeight);
+
+  if (hiddenElement.clientHeight === 0) {
+    hiddenElement.style.height = hiddenHeight + "px";
+  } else {
+    hiddenElement.style.height = "0px";
+  }
+}
+
+expandHiddenProjects();
